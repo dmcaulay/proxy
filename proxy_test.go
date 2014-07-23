1eca0522111151f4eca59b02d2d795a718253787
Dan McAulay <dmcaulay@gmail.com>
2014-07-23 12:42:12 -0700

    refactor tests

--- proxy_test.go
+++ proxy_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net"
 	"testing"
 )
@@ -10,7 +9,7 @@ var serverMap map[string]*net.UDPConn = make(map[string]*net.UDPConn)
 var c config
 var initialized bool = false
 
-func setup_test() {
+func setup_test(t *testing.T) {
 	if initialized {
 		return
 	}
@@ -18,19 +17,19 @@ func setup_test() {
 	initialized = true
 	err := c.read("test")
 	if err != nil {
-		log.Fatal(err)
+		t.Error("unable to read config file", err)
 	}
 
 	setup(c.Nodes)
-	makeServers()
+	makeServers(t)
 	go startServer(c.UdpVersion, c.Port, c.Host)
 }
 
-func makeServers() {
+func makeServers(t *testing.T) {
 	for _, n := range c.Nodes {
 		conn, err := makeConn(c.UdpVersion, n.Port, n.Host)
 		if err != nil {
-			log.Fatal(err)
+			t.Error("should be able to setup the servers", err)
 		}
 		serverMap[n.Name()] = conn
 	}
@@ -39,24 +38,24 @@ func makeServers() {
 func serverConn(t *testing.T) (net.PacketConn, net.UDPAddr) {
 	conn, err := net.ListenPacket(c.UdpVersion, "127.0.0.1:0")
 	if err != nil {
-		t.Error("should be able to create a connection")
+		t.Error("should be able to create a connection", err)
 	}
 	addr := makeAddr(c.Port, "127.0.0.1")
 	return conn, addr
 }
 
 func TestSetup(t *testing.T) {
-	setup_test()
+	setup_test(t)
 	name, err := cons.Get("statsd.metric.test")
 	if err != nil {
-		t.Error("cons should not return an error")
+		t.Error("cons should not return an error", err)
 	}
 	if name != "127.0.0.1:8129" {
 		t.Error("expected name to be 127.0.0.1:8129, but it was", name)
 	}
 	name, err = cons.Get("statsd.metric.name")
 	if err != nil {
-		t.Error("cons should not return an error")
+		t.Error("cons should not return an error", err)
 	}
 	if name != "127.0.0.1:8131" {
 		t.Error("expected name to be 127.0.0.1:8131, but it was", name)
@@ -64,22 +63,22 @@ func TestSetup(t *testing.T) {
 }
 
 func TestOneMetric(t *testing.T) {
-	setup_test()
+	setup_test(t)
 	conn, addr := serverConn(t)
 	_, err := conn.WriteTo([]byte("statsd.metric.test:1|c"), &addr)
 	if err != nil {
-		t.Error("conn Write should not return an error")
+		t.Error("conn Write should not return an error", err)
 	}
 
 	readMetric("127.0.0.1:8129", "statsd.metric.test:1|c", t)
 }
 
 func TestMultipleMetrics(t *testing.T) {
-	setup_test()
+	setup_test(t)
 	conn, addr := serverConn(t)
 	_, err := conn.WriteTo([]byte("statsd.metric.test:1|c\nstatsd.metric.name:2|g"), &addr)
 	if err != nil {
-		t.Error("conn Write should not return an error")
+		t.Error("conn Write should not return an error", err)
 	}
 
 	readMetric("127.0.0.1:8129", "statsd.metric.test:1|c", t)
@@ -91,7 +90,7 @@ func readMetric(server string, metric string, t *testing.T) {
 	b := make([]byte, 1024)
 	n, _, err := node.ReadFromUDP(b)
 	if err != nil {
-		t.Error("server Read should not return an error")
+		t.Error("server Read should not return an error", err)
 	}
 	cmd := string(b[:n])
 	if cmd != metric {
